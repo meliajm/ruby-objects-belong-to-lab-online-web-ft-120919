44f0c0f6e7ff29be27132dc0698b3a1be5fe394f
melia miller <meliajm@gmail.com>
2019-12-17 03:47:05 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+# class Song
+#   attr_accessor :title, :artist 
+  
+#   def initialize(title)
+#     @title = title
+#   end
+  
+# end 
+
+class Song
+ 
+  attr_accessor :title, :artist
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+end
